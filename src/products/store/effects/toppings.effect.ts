dc4c440dab98273c4bc5ac23cb04c543d3506c63
Kenny <kenny-nguyen@hotmail.co.uk>
2026-01-27 22:42:12 +0000

    Add toppings effects to NgRx store
    Introduced ToppingsEffects for handling toppings-related actions in the NgRx store. Updated the effects index to include and export the new toppings effect.

---
+++ src/products/store/effects/toppings.effect.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+
+import { Effect, Actions } from "@ngrx/effects";
+import { of } from "rxjs/observable/of";
+import { map, catchError, switchMap } from "rxjs/operators";
+
+import * as toppingActions from "../actions/toppings.action";
+import * as fromServices from "../../services/toppings.service";
+
+@Injectable()
+export class ToppingsEffects {
+  constructor(
+    private actions$: Actions,
+    private toppingsService: fromServices.ToppingsService,
+  ) {}
+
+  @Effect()
+  loadToppings$ = this.actions$.ofType(toppingActions.LOAD_TOPPINGS).pipe(
+    switchMap(() => {
+      return this.toppingsService.getToppings().pipe(
+        map((toppings) => new toppingActions.LoadToppingsSuccess(toppings)),
+        catchError((error) => of(new toppingActions.LoadToppingsFail(error))),
+      );
+    }),
+  );
+}
