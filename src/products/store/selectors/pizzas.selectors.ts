ce618a8d11f72ca5ce710fac110cea28339f3332
Kenny <kenny-nguyen@hotmail.co.uk>
2026-02-10 22:21:49 +0000

    Add toppings visualisation state and selectors
    Introduce VISUALISE_TOPPINGS action and VisualiseToppings action class; extend ToppingsAction union. Add selectedToppings to ToppingsState with initial empty array and reducer case to update it. Expose getSelectedToppings selector and wire it into pizzas.selectors by adding getPizzaVisualised, which composes the selected topping IDs with topping entities to produce a pizza object with its visualised toppings.

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -3,17 +3,18 @@ import { createSelector } from "@ngrx/store";
 import * as fromRoot from "../../../app/store";
 import * as fromFeature from "../reducers";
 import * as fromPizzas from "../reducers/pizzas.reducer";
+import * as fromToppings from "./toppings.selectors";
 
 import { Pizza } from "../../models/pizza.model";
 
 export const getPizzaState = createSelector(
   fromFeature.getProductsState,
-  (state: fromFeature.ProductsState) => state.pizzas
+  (state: fromFeature.ProductsState) => state.pizzas,
 );
 
 export const getPizzasEntities = createSelector(
   getPizzaState,
-  fromPizzas.getPizzasEntities
+  fromPizzas.getPizzasEntities,
 );
 
 export const getSelectedPizza = createSelector(
@@ -21,7 +22,17 @@ export const getSelectedPizza = createSelector(
   fromRoot.getRouterState,
   (entities, router): Pizza => {
     return router.state && entities[router.state.params.pizzaId];
-  }
+  },
+);
+
+export const getPizzaVisualised = createSelector(
+  getSelectedPizza,
+  fromToppings.getToppingEntities,
+  fromToppings.getSelectedToppings,
+  (pizza, toppingEntities, selectedToppings) => {
+    const toppings = selectedToppings.map((id) => toppingEntities[id]);
+    return { ...pizza, toppings };
+  },
 );
 
 export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
@@ -30,9 +41,9 @@ export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
 
 export const getPizzasLoaded = createSelector(
   getPizzaState,
-  fromPizzas.getPizzasLoaded
+  fromPizzas.getPizzasLoaded,
 );
 export const getPizzasLoading = createSelector(
   getPizzaState,
-  fromPizzas.getPizzasLoading
+  fromPizzas.getPizzasLoading,
 );
